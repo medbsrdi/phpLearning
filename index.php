21b222f1e4756b7d0d95b0ef6d4b315683d50c7a
Mohammed BOUSSARDI <66888805+medbsrdi@users.noreply.github.com>
2021-11-24 22:02:18 +0100

    string
    string function to back to it

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $nam = "hello world quick"; 
+        echo strtoupper($nam)."<br>"; # HELLO WORLD QUICK
+        echo strtolower($nam)."<br>"; # hello world quick
+        echo ucfirst($nam)."<br>"; # Hello world quick
+        echo ucwords($nam)."<br>"; # Hello World Quick
+        echo strstr($nam,"ll")."<br>"; # llo world quick
+        echo str_replace("quick","super fast",$nam)."<br>"; # hello world super fast
+        echo str_repeat($nam,2)."<br>"; # hello world quickhello world quick
+        echo substr($nam,5,10)."<br>"; # world qui
+        echo strchr($nam,"o")."<br>"; # o world quick
+    ?>
+</body>
+</html>
